875630cdf70847bc3376dc0e43a358fed652a2c0
AsadDUET <asadouzzaman@gmail.com>
2018-03-09 02:26:21 +0600

    basic responce to call is complete

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import speech_recognition as sr
+from gtts import gTTS
+from pygame import mixer # Load the required library
+import os
+
+mixer.init()
 r=sr.Recognizer()
 
 with sr.Microphone() as source:
@@ -6,6 +11,30 @@ with sr.Microphone() as source:
     audio = r.listen(source)
 
 try:
-    print(r.recognize_google(audio_data=audio,language="bn-BN"))
+    sps=''
+    sps=r.recognize_google(audio_data=audio,language="bn-BN")
+    print("1")
+    print(sps)
+    if (sps=="কেমন আছো"):
+        print("Ami valo asi")
+        tts = gTTS(text="আমি ভালো আছি । আপনি কেমন আছেন?", lang='bn')
+        print("1")
+        try:
+            tts.save("test.mp3")
+        except:
+            print("can't save")
+        print("3")
+        
+        mixer.music.load('test.mp3')
+        mixer.music.play()
+        while(mixer.music.get_busy()):
+            pass
+        mixer.music.load('test2.mp3')
+        print("done")
 except:
     pass
+
+
+
+
+
